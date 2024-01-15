160099b12d8d8ac877cacddeebb813ef707f6aa2
danielapgomes <danielapgomes@ua.pt>
2024-01-15 21:34:49 +0000

    I have revised the FAQ page. Previously, it featured a simple accordion with older questions designed in Figma. These questions were outdated. I have since updated them with current inquiries and added associated colors.

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -1,76 +1,243 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Accordion from 'react-bootstrap/Accordion';
 import NavBar from '../globalComponents/NavBar';
+import './FAQs.css';
 
 function FAQs() {
+  const circleStyle1 = {
+    height: '50px',
+    width: '50px',
+    backgroundColor: '#EEAD6D',
+    borderRadius: '50%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '10px',
+  };
+
+  const circleStyle2 = {
+    height: '50px',
+    width: '50px',
+    backgroundColor: '#EDD57F',
+    borderRadius: '50%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '10px',
+  };
+
+  const circleStyle3 = {
+    height: '50px',
+    width: '50px',
+    backgroundColor: '#61BEAD',
+    borderRadius: '50%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '10px',
+  };
+
+  const circleStyle4 = {
+    height: '50px',
+    width: '50px',
+    backgroundColor: '#628FC5',
+    borderRadius: '50%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '10px',
+  };
+
+  const circleStyle5 = {
+    height: '50px',
+    width: '50px',
+    backgroundColor: '#B39AC7',
+    borderRadius: '50%',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '10px',
+  };
+
+  const [openItem, setOpenItem] = useState(null);
+
+  const toggleItem = (eventKey) => {
+    setOpenItem(openItem === eventKey ? null : eventKey);
+  };
+  
   return (
     <div>
       <NavBar />
-      <Accordion defaultActiveKey="1">
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Q1: How can I identify sustainable clothing?</Accordion.Header>
+      <div className="accordion-container">
+      <h2 style={{ textAlign: 'left', marginBottom: '20px' }}>FAQs - Identifying Sustainable Clothing and Certifications</h2>
+        <Accordion defaultActiveKey="1" onSelect={toggleItem}>
+        <Accordion.Item eventKey="1">
+        <Accordion.Header>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {openItem !== "1" && ( 
+                <span style={{ ...circleStyle1, height: '25px', width: '25px', flexShrink: 0 }}></span>
+            )}
+            <div style={{ textAlign: 'left', marginLeft: '10px' }}>
+              1. Organic Material Certification
+            </div>
+          </div>
+        </Accordion.Header>
+
         <Accordion.Body>
-        Our platform ensures the identification of sustainable clothing through various certifications. Look for labels such as GOTS, Fair Trade, Bluesign, and others mentioned in our terms and conditions. These certifications guarantee environmentally friendly production, fair working conditions, and ethical practices throughout the supply chain.
+          <div style={{ textAlign: 'Left' }}>
+            <span style={circleStyle1}></span>
+            <div>
+              <p><strong>GOTS (Global Organic Textiles Standard):</strong><br />
+                Certifies products with at least 70% organic agriculture contribution.<br />
+                Ensures environmentally friendly production, better working conditions, and quality assurance.<br />
+                Prohibits hazardous inputs like toxic heavy metals and aromatic solvents.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://global-standard.org/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                  </div>
+                  <br />
+              <p><strong>OCS (Organic Content Standard):</strong><br />
+                Enables the use of organic material in processing.<br />
+                Validates the presence and amount of organic material in the final product.<br />
+                Applies to fibers, wool, fabrics, clothes, and upholstery fabrics, excluding leather.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://textileexchange.org/organic-content-standard/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                </div>
+            </div>
+          </div>
         </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Q2: What is GOTS certification, and how does it contribute to sustainability?</Accordion.Header>
-        <Accordion.Body>
-        The Global Organic Textile Standard (GOTS) certification ensures that textile products have a minimum of 70% organic agriculture contribution. It guarantees environmentally friendly production, better working conditions, and the prohibition of hazardous inputs such as toxic heavy metals and aromatic solvents.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="3">
-        <Accordion.Header>Q3: What does the Fair Trade certificate signify in the textile industry?</Accordion.Header>
-        <Accordion.Body>
-        The Fair Trade certificate, issued by the World Fair Trade Organisation, ensures sustainable and equitable trade relationships. It focuses on fair working conditions, primarily using fair-trade cotton, and applies to end products exported from developing to developed countries. 
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="4">
-        <Accordion.Header>Q4: How does Bluesign contribute to sustainable textile production?</Accordion.Header>
-        <Accordion.Body>
-        Bluesign certification emphasizes the responsible use of resources and minimal impact on people and the environment. It combines consumer safety, water and air emissions, and occupational health, ensuring the final textile product meets stringent consumer safety requirements worldwide.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="5">
-        <Accordion.Header>Q5: What is SA8000 certification, and how does it promote social standards?</Accordion.Header>
-        <Accordion.Body>
-        SA8000 is a leading social certification program ensuring a fair and decent environment for workers. It checks for criteria such as child labor, health, and safety, freedom of association, and right to collective bargaining, promoting the highest social standards.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="6">
-        <Accordion.Header>Q6: How does the Zero Discharge of Hazardous Chemicals (ZDHC) certification improve environmental conditions?</Accordion.Header>
-        <Accordion.Body>
-        ZDHC aims to treat toxic substances in the textile, leather, and footwear value chain, assuring safer health conditions. It involves signatory brands, value chain affiliates, and associates to improve the environment and people’s wellbeing.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="7">
-        <Accordion.Header>Q7: What does Responsible Wool Standard (RWS) certification guarantee in wool sourcing?</Accordion.Header>
-        <Accordion.Body>
-        RWS focuses on proper treatment of sheep and maintaining the quality of land they graze on. It provides verification of practices at the farm level, ensuring the best quality of wool following global standards.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="8">
-        <Accordion.Header>Q8: How does the Recycled Claim Standard (RCS) contribute to sustainable fashion?</Accordion.Header>
-        <Accordion.Body>
-        RCS is a chain of custody standard tracking recycled raw materials in the supply chain. It ensures minimal wastage in processing stages and checks for the presence and amount of recycled material in the final product.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="9">
-        <Accordion.Header>Q9: What is the Sustainable Fibre Alliance (SFA) and its role in the cashmere industry?</Accordion.Header>
-        <Accordion.Body>
-        SFA's Sustainable Cashmere Standard tackles sustainability challenges in cashmere production, focusing on the safety and welfare of goats and herders. It is the world’s first holistic sustainability cashmere standard.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="10">
-        <Accordion.Header>Q10: How does Cradle to Cradle certification contribute to eco-intelligent product design?</Accordion.Header>
-        <Accordion.Body>
-        Cradle to Cradle certification is a multi-attribute label showcasing efforts in eco-intelligent product design. It focuses on eco-friendly and recyclable materials, efficient use of water and energy, making it applicable from raw materials to finished products.
-        For more details on certifications and sustainable practices, refer to our terms and conditions and the provided references.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>        
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              {openItem !== "2" && ( 
+                  <span style={{ ...circleStyle2, height: '25px', width: '25px', flexShrink: 0 }}></span>
+              )}
+              <div style={{ textAlign: 'left', marginLeft: '10px' }}>
+                2. Fair Trade and Ethical Manufacturing:
+              </div>
+            </div>
+          </Accordion.Header>
+          <Accordion.Body>
+          <div style={{ textAlign: 'Left' }}>
+            <span style={circleStyle2}></span>
+            <div>
+              <p><strong>Fair Trade Textile Standard:</strong><br />
+                Facilitates sustainable and equitable trade relationships.<br />
+                Applicable for end products exported from developing to developed countries.<br />
+                Ensures fair-trade cotton usage and safe working conditions in the supply chain.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://www.fairtrade.net/standard/textile" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                  </div>
+                  <br />
+              <p><strong>WRAP (Worldwide Responsible Apparel Production):</strong><br />
+                Independent certification fostering safe, lawful, humane, and ethical manufacturing.<br />
+                Demonstrates a commitment to social and ethical standards.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://wrapcompliance.org/en/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                </div>
+            </div>
+          </div>
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {openItem !== "3" && ( 
+                <span style={{ ...circleStyle3, height: '25px', width: '25px', flexShrink: 0 }}></span>
+            )}
+              <div style={{ textAlign: 'left', marginLeft: '10px' }}>
+                3. Environmental Impact and Resource Responsibility:
+              </div>
+          </div>
+            </Accordion.Header>
+          <Accordion.Body>
+          <div style={{ textAlign: 'Left' }}>
+            <span style={circleStyle3}></span>
+            <div>
+              <p><strong>Bluesign:</strong><br />
+                Emphasizes responsible resource use and minimal impact on people and the environment.<br />
+                Certifies stringent consumer safety requirements and promotes an ideal production process.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://www.bluesign.com/en/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                  </div>
+                  <br />
+              <p><strong>SFA (Sustainable Fibre Alliance):</strong><br />
+                Tackles sustainability challenges in cashmere production.<br />
+                Focuses on the safety and welfare of goats and herders.<br />
+                Developed as the world's first holistic sustainability cashmere standard.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://sustainablefibre.org/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                </div>
+            </div>
+          </div>
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="4">
+          <Accordion.Header>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {openItem !== "4" && ( 
+                <span style={{ ...circleStyle4, height: '25px', width: '25px', flexShrink: 0 }}></span>
+            )}
+              <div style={{ textAlign: 'left', marginLeft: '10px' }}>
+                4. Animal Welfare Certification:
+              </div>
+          </div>
+            </Accordion.Header>
+          <Accordion.Body>
+          <div style={{ textAlign: 'Left' }}>
+            <span style={circleStyle4}></span>
+            <div>
+              <p><strong>RWS (Responsible Wool Standard):</strong><br />
+                Ensures the proper treatment of sheep and maintains the quality of grazing land.<br />
+                Provides verification of farm-level practices, allowing brands to claim sustainable wool sourcing.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://textileexchange.org/responsible-wool-standard/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                  </div>
+                  <br />
+              <p><strong>RDS (Responsible Down Standard):</strong><br />
+                Ensures animal welfare in down and feather products.<br />
+                Examines various animal products to guarantee their welfare.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://textileexchange.org/responsible-down-standard/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                </div>
+            </div>
+          </div>
+          </Accordion.Body>
+        </Accordion.Item>
+
+        <Accordion.Item eventKey="5">
+          <Accordion.Header>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {openItem !== "5" && ( 
+                <span style={{ ...circleStyle5, height: '25px', width: '25px', flexShrink: 0 }}></span>
+            )}
+              <div style={{ textAlign: 'left', marginLeft: '10px' }}>
+                5. Textile Industry Safety and Workplace Conditions:
+              </div>
+          </div>
+          </Accordion.Header>
+          <Accordion.Body>
+          <div style={{ textAlign: 'Left' }}>
+            <span style={circleStyle5}></span>
+            <div>
+              <p><strong>FWF (Fair Wear Foundation):</strong><br />
+                Works for safer working conditions and sustainable workplace improvements.<br />
+                Brands can prioritize manufacturers certified by FWF for safe working conditions.</p>
+                <div className="d-flex justify-content-center">
+                  <a href="https://www.fairwear.org/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">More</a>
+                </div>
+            </div>
+          </div>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <p style={{ marginTop: '20px' }}>These categories cover a range of certifications addressing environmental impact, ethical manufacturing, animal welfare, and workplace conditions in the textile industry.</p>
     </div>
-    
+    </div>
   );
 }
 
